2c621a21489a7e81fd9f27107ba5cb96e1185be9
MGylden <99180585+MGylden@users.noreply.github.com>
2022-12-15 17:54:09 +0100

    Add files via upload

---
+++ mytube/Models/PlaylistItem.cs
@@ -0,0 +1,15 @@
+﻿namespace mytube.Models
+{
+    public class PlaylistItem
+    {
+        public int Id { get; set; }
+        public string PlaylistName { get; set; }
+        
+        public string PlaylistDescription { get; set; }
+        public string PlaylistType { get; set;}
+        public string PlaylistUrl { get; set; }
+        public string PlaylistTitle { get; set;}
+        public ICollection<VideoItem>? Video { get; set; }
+        public ICollection<UserItem>? User { get; set; }
+    }
+}
